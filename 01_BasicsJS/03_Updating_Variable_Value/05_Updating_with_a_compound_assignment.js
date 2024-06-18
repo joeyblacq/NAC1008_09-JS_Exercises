ddd7aa0366a4d9251f7f8963d03da238327b2fd9
joel jackson <jacksonjo360@gmail.com>
2024-06-18 12:23:21 -0700

    kut

--- 01_BasicsJS/03_Updating_Variable_Value/05_Updating_with_a_compound_assignment.js
+++ 01_BasicsJS/03_Updating_Variable_Value/05_Updating_with_a_compound_assignment.js
@@ -2,12 +2,12 @@
 
 // Declare a 'let' variable named 'totalScore' with the value 100.
 // Write your code below
-
+let totalScore = 100;
 
 // Update the value of 'totalScore' by multiplying it by 1.5 using the compound assignment (*= operator).
 // Write your code below
 
-
+totalScore  *= 1.5;
 
 console.log(`The total score is: ${totalScore}`);
 // Expected Output: The total score is: 150
@@ -16,4 +16,4 @@ console.log(`The total score is: ${totalScore}`);
 
 
 // In the terminal, copy & paste the code below and run it to see the result in the console.
-// node 01_BasicsJS/03_Updating_Variable_Value/05_Updating_with_a_Compound_Assignment.js
+// node 01_BasicsJS/03_Updating_Variable_Value/05_Updating_with_a_compound_assignment.js
