a43a0f738f9c05278f32d5394a9f92acd1969f9b
joel jackson <jacksonjo360@gmail.com>
2024-06-18 14:36:30 -0700

    yoo

--- 01_BasicsJS/04_String_Concatenation/03_Combine_strings_with_a_number.js
+++ 01_BasicsJS/04_String_Concatenation/03_Combine_strings_with_a_number.js
@@ -2,15 +2,15 @@
 
 // Step 1: Declare a variable named 'text' with the string value of 'The score is '.
 // Write your code below
-
+let text ='The score is';
 
 // Step 2: Declare another variable named 'score' with the value 100.
 // Write your code below
-
+let score = 100;
 
 // Step 3: Declare another variable named 'message' and assign it the value of the formula: text + score.
 // Write your code below
-
+let message = text + score
 
 
 console.log(message);
