03bb4e5684d5757dd2f19be801925e2b6074e93f
joel jackson <jacksonjo360@gmail.com>
2024-06-19 14:21:34 -0700

    gee

--- 01_BasicsJS/07_Arithmetic_Operators/03_Multiplication.js
+++ 01_BasicsJS/07_Arithmetic_Operators/03_Multiplication.js
@@ -2,12 +2,15 @@
 
 // Step 1: Declare two variables, 'numA' with the value 5 and 'numB' with the value 6.
 // Write your code below
+let numA = 5;
+let numB = 6;
 
 
 // Step 2: Declare a variable named 'result' that equals the product of 'numA' and 'numB'.
     // Hint: 'You can multiply numbers with the '*' operator.'
 // Write your code below
 
+let result = numA * numB;
 
 
 console.log(`The result of 'numA' * 'numB' is: ${result}`);
