189addbdad626d880d11bb846dd4cc3a470d3b99
joel jackson <jacksonjo360@gmail.com>
2024-06-19 14:50:45 -0700

    ye

--- 01_BasicsJS/08_Comparison_Operators/01_Equal_Operator.js
+++ 01_BasicsJS/08_Comparison_Operators/01_Equal_Operator.js
@@ -2,12 +2,14 @@
 
 // Step 1: Declare two variables, 'numX' with the value 10 and 'numY' with the value 10.
 // Write your code below
-
+const numX = 10;
+const numY = 10;
 
 // Step 2: Declare a variable named 'isEqual' and 
     // use the equal operator '==' to check if 'numX' and 'numY' are equal.
 // Write your code below
 
+const isEqual = numX == numY;
 
 
 console.log(isEqual);
