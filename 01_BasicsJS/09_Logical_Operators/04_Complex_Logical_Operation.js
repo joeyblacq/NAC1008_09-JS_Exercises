fe21d71370ae46dbadd503f84d766436fc2ca2d9
joel jackson <jacksonjo360@gmail.com>
2024-06-20 11:04:20 -0700

    y

--- 01_BasicsJS/09_Logical_Operators/04_Complex_Logical_Operation.js
+++ 01_BasicsJS/09_Logical_Operators/04_Complex_Logical_Operation.js
@@ -2,13 +2,16 @@
 
 // Step 1: Declare three variables: 'isDaytime' with the value 'true', 'isWeekend' with the value 'false', and 'scheduledMeeting' with the value 'true'.
 // Write your code below
-
+const isDaytime = true;
+const isWeekend = false;
+const hasMeeting = true
 
 // Step 2: Declare a variable named 'canAttendMeeting' and 
     // use the logical AND operator '&&' to check if 'isDaytime' and 'isWeekend' is both true and 'hasMeeting' is true.
     // Hint: Use the logical NOT operator '!' on 'isWeekend'
 // Write your code below
 
+const canAttendMeeting = isDaytime && ! isWeekend && hasMeeting;
 
 
 console.log(canAttendMeeting);
