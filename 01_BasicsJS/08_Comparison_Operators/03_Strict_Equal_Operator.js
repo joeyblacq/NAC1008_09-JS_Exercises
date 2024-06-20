43f546d0f9e7a5e1548efb2702998e4a3f10788a
joel jackson <jacksonjo360@gmail.com>
2024-06-20 10:02:40 -0700

    momo

--- 01_BasicsJS/08_Comparison_Operators/03_Strict_Equal_Operator.js
+++ 01_BasicsJS/08_Comparison_Operators/03_Strict_Equal_Operator.js
@@ -3,11 +3,13 @@
 // Step 1: Declare two variables, 'numC' with the value 5 and 'stringNumC' with the value '5'.
 // Write your code below
 
+const numC = 5;
+const stringNumC ='5'
 
 // Step 2: Declare a variable named 'isStrictEqual' and 
     // use the strict equal operator '===' to check if 'numC' and 'stringNumC' are equal in both value and type.
 // Write your code below
-
+const isStrictEqual = (numC === stringNumC);
 
 console.log(isStrictEqual);
 // Expected Output: false
