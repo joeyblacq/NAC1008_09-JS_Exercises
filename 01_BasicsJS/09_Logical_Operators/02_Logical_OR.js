a0edd24ed5f55d140d837671259d9c49c15fd539
joel jackson <jacksonjo360@gmail.com>
2024-06-20 10:31:28 -0700

    a

--- 01_BasicsJS/09_Logical_Operators/02_Logical_OR.js
+++ 01_BasicsJS/09_Logical_Operators/02_Logical_OR.js
@@ -2,12 +2,13 @@
 
 // Step 1: Declare two variables, 'hasCoffee' with the value 'false' and 'isTired' with the value 'true'.
 // Write your code below
-
+const hasCoffee = false;
+const isTired = true;
 
 // Step 2: Declare a variable named 'needsBreak' and 
     // use the logical OR operator '||' to check if '!hasCoffee' or 'isTired' is true.
 // Write your code below
-
+const needsBreak = !hasCoffee||isTired
 
 
 console.log(needsBreak);
