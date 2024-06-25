21efaabe6908bfa93cfa9c8ec3598f187f9f2a17
joel jackson <jacksonjo360@gmail.com>
2024-06-25 11:26:06 -0700

    a

--- 02_ControlFlow/14_Switch_Statements/02_Switch_Statement_with_Numeric_Values.js
+++ 02_ControlFlow/14_Switch_Statements/02_Switch_Statement_with_Numeric_Values.js
@@ -2,25 +2,25 @@
 
 // Step 1: Declare a 'let 'variable 'number' with the function value: 'Math.floor(Math.random() * 3) + 1'.
 // Write your code below
-
+let number = Math.floor(Math.random() * 3) + 1
 
 // Step 2: Use a switch statement to log different messages based on the numeric value.
   // if it's 1, console.log(`The number is one.`)
   // if it's 2, console.log(`The number is two.`)
   // if it's 3, console.log(`The number is three.`)
 // Write your code below
-switch () {
-  case '':
-    
+switch (number) {
+  case 'num1':
+    console.log(`The number is one.`)
     break;
-  case '':
-    
+  case 'num2':
+    console.log(`The number is two.`)
     break;
-  case '':
-    
+  case 'num3':
+    console.log(`The number is three.`)
     break;
   default:
-
+    console.log("Invalid number.");
 }
 
 
--- 02_ControlFlow/14_Switch_Statements/03_Switch_Statement_with_Multiple_Cases.js
+++ 02_ControlFlow/14_Switch_Statements/03_Switch_Statement_with_Multiple_Cases.js
@@ -3,19 +3,25 @@
 // Step 1: Declare a variable 'fruit' with the value 'apple'.
 // Write your code below
 
+let fruit = 'apple';
 
 // Step 2: Use a switch statement to log different messages based on the fruit.
   // if it's a banana, console.log(`It's a banana.`)
   // if it's an apple or an orange, console.log(`It's an apple or an orange.`)
   // if it's any other fruit, console.log(`It's a different fruit.`)
 // Write your code below
-switch () {
-  case '':
-    
+switch (fruit) {
+  case 'banana':
+    console.log("It's a banana.")
+
     break;
-  case '':
-  case '':
-    
+
+  case 'apple':
+    case 'orange':
+    console.log("It's an apple or an orange.")
+
+  case 'fruit':
+    console.log("It's a different fruit.")
     break;
   default:
     
