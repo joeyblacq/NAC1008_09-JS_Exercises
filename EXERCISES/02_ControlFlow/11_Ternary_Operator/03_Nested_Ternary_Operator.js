9b2e2a373a5383763fcd6ec17dd1c8ad1b5fb253
joel jackson <jacksonjo360@gmail.com>
2024-08-15 10:43:23 -0700

    redo

--- EXERCISES/02_ControlFlow/11_Ternary_Operator/03_Nested_Ternary_Operator.js
+++ EXERCISES/02_ControlFlow/11_Ternary_Operator/03_Nested_Ternary_Operator.js
@@ -4,35 +4,30 @@
 // Write your code below
 
 let isWeekend = false;
-let time = 'morning';
-
+let time = "morning";
 
 // Step 2: Use nested ternary operators to set a variable 'greeting' to
-    // 'Enjoy your weekend' if it is the weekend,
-    // and if not, 
-        // set it to 'Good morning' if it is morning 
-        // and 'Good day' otherwise.
-    // Hint: Ternary operators are similar to if-else statements but written in a shorter way: 
-    // variable = (condition) ? value1 if true : (new condition) ? value2 if true : value3 if false
+// 'Enjoy your weekend' if it is the weekend,
+// and if not,
+// set it to 'Good morning' if it is morning
+// and 'Good day' otherwise.
+// Hint: Ternary operators are similar to if-else statements but written in a shorter way:
+// variable = (condition) ? value1 if true : (new condition) ? value2 if true : value3 if false
 // Write your code below
 
- let greeting = day == 'saturday' || day == 'sunday'?
-'enjoy your weekend':
-time =='morning'?
-'good moring':
-'good day';
- 
-
+let greeting = isWeekend
+  ? "Enjoy your weekend"
+  : time === "morning"
+  ? "Good morning"
+  : "Good day";
+  
 
 // Don't change the code of the console.log()
 console.log(greeting);
 // Expected Output: Good morning
 
-
-
-
 // To see the result of the console.log(), use the command below in the terminal:
 // node EXERCISES/02_ControlFlow/11_Ternary_Operator/03_Nested_Ternary_Operator.js
 
 // To run the test of this specific exercise, use the command below in the terminal:
-// npm run test:2.11.3 
+// npm run test:2.11.3
